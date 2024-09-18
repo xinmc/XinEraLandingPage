0658b85336aae0f898f6123e3a4153fc45eb72e7
King Star <mcxin.y@gmail.com>
2024-09-18 12:26:01 +0000

    Refactor features-planet component and update footer
    - Refactor the features-planet component to remove commented out code and improve code readability.
    - Update the footer component to add a QQ link and replace the Medium link with a GitHub link.

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -18,6 +18,59 @@ export default function FeaturesPlanet() {
             </h2>
           </div>
           {/* Planet */}
+{/*          <div className="pb-16 md:pb-20" data-aos="zoom-y-out">
+            <div className="text-center">
+              <div className="relative inline-flex rounded-full before:absolute before:inset-0 before:-z-10 before:scale-[.85] before:animate-[pulse_4s_cubic-bezier(.4,0,.6,1)_infinite] before:bg-gradient-to-b before:from-blue-900 before:to-sky-700/50 before:blur-3xl after:absolute after:inset-0 after:rounded-[inherit] after:[background:radial-gradient(closest-side,theme(colors.blue.500),transparent)]">
+                <Image
+                  className="rounded-full bg-gray-900"
+                  src={PlanetImg}
+                  width={400}
+                  height={400}
+                  alt="Planet"
+                />
+                <div className="pointer-events-none" aria-hidden="true">
+                  <Image
+                    className="absolute -right-64 -top-20 z-10 max-w-none"
+                    src={PlanetOverlayImg}
+                    width={789}
+                    height={755}
+                    alt="Planet decoration"
+                  />
+                  <div>
+                    <Image
+                      className="absolute -left-28 top-16 z-10 animate-[float_4s_ease-in-out_infinite_both] opacity-80 transition-opacity duration-500"
+                      src={PlanetTagImg01}
+                      width={253}
+                      height={56}
+                      alt="Tag 01"
+                    />
+                    <Image
+                      className="absolute left-56 top-7 z-10 animate-[float_4s_ease-in-out_infinite_1s_both] opacity-30 transition-opacity duration-500"
+                      src={PlanetTagImg02}
+                      width={241}
+                      height={56}
+                      alt="Tag 02"
+                    />
+                    <Image
+                      className="absolute -left-20 bottom-24 z-10 animate-[float_4s_ease-in-out_infinite_2s_both] opacity-25 transition-opacity duration-500"
+                      src={PlanetTagImg03}
+                      width={243}
+                      height={56}
+                      alt="Tag 03"
+                    />
+                    <Image
+                      className="absolute bottom-32 left-64 z-10 animate-[float_4s_ease-in-out_infinite_3s_both] opacity-80 transition-opacity duration-500"
+                      src={PlanetTagImg04}
+                      width={251}
+                      height={56}
+                      alt="Tag 04"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> */}
+          {/* 一个MC方块显示在中间，边缘环绕几个角色头 */}
           <div className="pb-16 md:pb-20" data-aos="zoom-y-out">
             <div className="text-center">
               <div className="relative inline-flex rounded-full before:absolute before:inset-0 before:-z-10 before:scale-[.85] before:animate-[pulse_4s_cubic-bezier(.4,0,.6,1)_infinite] before:bg-gradient-to-b before:from-blue-900 before:to-sky-700/50 before:blur-3xl after:absolute after:inset-0 after:rounded-[inherit] after:[background:radial-gradient(closest-side,theme(colors.blue.500),transparent)]">
--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -113,30 +113,21 @@ export default function Footer({ border = false }: { border?: boolean }) {
               <li>
                 <Link
                   className="flex items-center justify-center text-blue-500 transition hover:text-blue-600"
-                  href="#0"
-                  aria-label="Twitter"
+                  href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=cTOsRPl13F0Rl2tmyXIXoXbuWk0ffZPA&authKey=CEkJPdxuZQSjWg7mS4b0uNuO%2Fg10kQcysVgKbWJEvRCBgYH7xVMWcMjEowg%2FAqbc&noverify=0&group_code=874882992"
+                  aria-label="QQ"
                 >
+                  <img
+                  className="h-8 w-8 fill-current"
+                  src="/images/qq.png"
+                  alt="QQ"
+                  style={{ transform: "scale(0.65)" }}
+                  />
                 </Link>
               </li>
               <li>
                 <Link
                   className="flex items-center justify-center text-blue-500 transition hover:text-blue-600"
-                  href="#0"
-                  aria-label="Medium"
-                >
-                  <svg
-                    className="h-8 w-8 fill-current"
-                    viewBox="0 0 32 32"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M23 8H9a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V9a1 1 0 0 0-1-1Zm-1.708 3.791-.858.823a.251.251 0 0 0-.1.241V18.9a.251.251 0 0 0 .1.241l.838.823v.181h-4.215v-.181l.868-.843c.085-.085.085-.11.085-.241v-4.887l-2.41 6.131h-.329l-2.81-6.13V18.1a.567.567 0 0 0 .156.472l1.129 1.37v.181h-3.2v-.181l1.129-1.37a.547.547 0 0 0 .146-.472v-4.749a.416.416 0 0 0-.138-.351l-1-1.209v-.181H13.8l2.4 5.283 2.122-5.283h2.971l-.001.181Z"></path>
-                  </svg>
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="flex items-center justify-center text-blue-500 transition hover:text-blue-600"
-                  href="#0"
+                  href="https://github.com/xinmc"
                   aria-label="Github"
                 >
                   <svg
